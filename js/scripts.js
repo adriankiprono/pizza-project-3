69844f2d70415dfbb7f53b628a32852d6ca9db85
adriankiprono <tuimuradrian6@gmail.com>
2019-10-14 08:34:52 +0300

    add constructor

--- js/scripts.js
+++ js/scripts.js
@@ -376,6 +376,50 @@ $(function () {
                 break;
 
         }
+        let newOrder = order(flavour, size, crust, topping, number, totalPrice);
+        console.log(newOrder);
+
+
+        $('.summary').slideDown(2000);
+        $('.cdata-overlay').slideUp();
+        $('#list').slideDown();
+        $('.deliver').show(1000);
+        $('.delivernot').show(1000);
+
+        $('#list').text(" ");
+        $("#list").append("<br>" + "Flavour :   " + newOrder.f + "<br>" + "Size :   "
+            + newOrder.s + "<br>" + "Crust :     "
+            + newOrder.c + "<br>" + "Toppings :     "
+            + newOrder.t + "<br>" + " Number of pizzas :    "
+            + newOrder.n + "<br>" + "Total Price :  "
+            + newOrder.total + "<br><br>").css('font-family', 'system-ui').css('font-size', '24px');
+    });
+
+
+    $(".deliver").click(function () {
+        $('.summary').slideUp();
+        $('#list').slideUp();
+        $('.summary').text("Provide location details").slideDown();
+        $('.deliver').hide(1000);
+        $('.delivernot').hide(1000);
+        $('.cdata-overlay').slideDown();
+    });
+
+
+
+
+    $(".delivernot").click(function () {
+
+    });
+
+
+    $(function () {
+        $.scrollify.move('#sum-order');
+    });
+
+
+});
+
     
 
 
